be4d7980dbff4adaf3cad46b9b8b5d1441764688
michaelserupmj <michaelserupmj@gmail.com>
2018-10-08 12:37:46 +0200

    Rettelse fra frelæggelse

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SmartMenuLibrary
 {
@@ -14,7 +15,8 @@ namespace SmartMenuLibrary
                 }
                 else
                 {
-                    //System.IO.StreamReader sr = new System.IO.StreamReader(@"../../../SmartMenuLibrary/" + path); // Åbner for filen
+                System.IO.StreamReader sr = 
+                    new System.IO.StreamReader(@"../../../SmartMenuLibrary/" + "MenuSpecDK.txt"); // Åbner for filen
 
                     string line = null;
                     int count = 1;
